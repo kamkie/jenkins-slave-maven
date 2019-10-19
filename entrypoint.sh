2914c636f5b58467004fa58cee68c9c61904d961
Kamil Kiewisz <kamkie@outlook.com>
2019-10-19 20:59:33 +0200

    run agent

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 #/usr/local/bin/run-jnlp-client "$@"
 source /usr/local/bin/run-jnlp-client
-java -jar agent.jar -jnlpUrl https://jenkins-kamkie.apps.openshift.oc-test.jit.ninja/computer/slave/slave-agent.jnlp -secret 4cf80c5fd6f8235dd0421e99b63d9472ed4c21057e5010e7ff8a20d3048c1489 -workDir "/home/jenkins"
+java -jar agent.jar -jnlpUrl ${JENKINS_URL}computer/slave/slave-agent.jnlp -secret ${JENKINS_SECRET} -workDir "/home/jenkins" -tunnel $JENKINS_TUNNEL
