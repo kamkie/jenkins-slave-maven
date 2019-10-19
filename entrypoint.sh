75142a8eec0ead47b99276e2dd1bde8b2a62510a
Kamil Kiewisz <kamkie@outlook.com>
2019-10-19 20:45:28 +0200

    run agent

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
-sudo dockerd -H unix:///var/run/docker.sock -H tcp://127.0.0.1:2375 &
+#sudo dockerd -H unix:///var/run/docker.sock -H tcp://127.0.0.1:2375 &
 
-/usr/local/bin/run-jnlp-client "$@"
+#/usr/local/bin/run-jnlp-client "$@"
+source /usr/local/bin/run-jnlp-client
+java -jar agent.jar -jnlpUrl https://jenkins-kamkie.apps.openshift.oc-test.jit.ninja/computer/slave/slave-agent.jnlp -secret 4cf80c5fd6f8235dd0421e99b63d9472ed4c21057e5010e7ff8a20d3048c1489 -workDir "/home/jenkins"
